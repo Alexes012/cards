111d89b8b5d3154ffbf07e0b09ebfc93c28c7698
Alexei <61819719+Alexes012@users.noreply.github.com>
2020-07-23 19:52:08 +0300

    Updates

---
+++ precache-manifest.3883e93b0e800b6a454e8072d6ccdd12.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a318e32895c16de65ca3484454cc6c0",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "9fd2937c38c9e26afb0c",
+    "url": "/cards/static/css/main.60d361d5.chunk.css"
+  },
+  {
+    "revision": "46868626ed1f546910c9",
+    "url": "/cards/static/js/2.9a48f342.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/cards/static/js/2.9a48f342.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fd2937c38c9e26afb0c",
+    "url": "/cards/static/js/main.01d78d79.chunk.js"
+  },
+  {
+    "revision": "c75bec67d5cd4571c3a8",
+    "url": "/cards/static/js/runtime-main.ae979815.js"
+  },
+  {
+    "revision": "7d2881bc491ea2dc7b906dde51bbecde",
+    "url": "/cards/static/media/friday.7d2881bc.jpg"
+  }
+]);
